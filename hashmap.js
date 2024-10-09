6b6a5e0c7429a8c45bd48b33b394a37ab7742235
Thielison <67246254+thielison@users.noreply.github.com>
2024-10-09 20:47:00 -0300

    Add methods to LinkedList and HashMap classes

--- hashmap.js
+++ hashmap.js
@@ -1,9 +1,11 @@
+import LinkedList from "./linkedlist.js";
+
 class HashMap {
     constructor(size = 16) {
-        // Using a loop to ensure each sub-array is a unique reference
-        this.buckets = Array.from({ length: size }, () => []);
-        this.capacity = 0;
+        // Each bucket will be a Linked List to handle collisions independently
+        this.buckets = Array.from({ length: size }, () => new LinkedList());
         this.loadFactor = 0.75;
+        this.numberOfEntries = 0;
     }
 
     hash(key) {
@@ -19,27 +21,51 @@ class HashMap {
 
     set(key, value) {
         const index = this.hash(key);
+        const linkedList = this.buckets[index];
 
-        this.buckets[index].push({ key, value });
-        this.capacity += 1;
+        if (linkedList.contains(key)) {
+            linkedList.update(key, value);
+        } else {
+            linkedList.append(key, value);
 
-        this.buckets[index].forEach((obj) => {
-            console.log(obj.key);
-        });
+            this.numberOfEntries += 1;
+        }
+    }
+
+    // Returns the string representation of the bucket (linked list) for the given key
+    getBucketAsString(key) {
+        const index = this.hash(key);
+        const linkedList = this.buckets[index];
+        const string = linkedList.toString();
+
+        return string;
+    }
+
+    numberOfBuckets() {
+        console.log(`Number of buckets: ${this.buckets.length}`);
     }
 }
 
 const test = new HashMap();
 
-test.set("apple", "red");
-test.set("banana", "yellow");
-test.set("carrot", "orange");
-test.set("dog", "brown");
-test.set("elephant", "gray");
-test.set("frog", "green");
+// Same bucket
 test.set("grape", "purple");
 test.set("hat", "black");
+
+test.set("dog", "brown");
+test.set("lion", "golden");
+
+// Different buckets
+test.set("elephant", "gray");
+test.set("frog", "green");
 test.set("ice cream", "white");
 test.set("jacket", "blue");
 test.set("kite", "pink");
-test.set("lion", "golden");
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+
+console.log(test);
+
+test.numberOfBuckets();
+console.log(test.getBucketAsString("dog"));
--- linkedlist.js
+++ linkedlist.js
@@ -1,13 +1,91 @@
 class Node {
-    constructor(value = null, nextNode = null) {
+    constructor(key = null, value = null, nextNode = null) {
+        this.key = key;
         this.value = value;
         this.nextNode = nextNode;
     }
 }
 
-class linkedList {
+class LinkedList {
     constructor() {
-        this.head = new Node();
-        this.size = 0;
+        this.head = null;
+        this.listSize = 0;
+    }
+
+    // Append a new node to the Linked List
+    append(key, value) {
+        const node = new Node(key, value);
+
+        // If the list is empty, this new node will be set as the head of the Linked List
+        if (!this.head) {
+            this.head = node;
+        } else {
+            // Traverse to the end of the list
+            let current = this.head;
+
+            while (current.nextNode) {
+                current = current.nextNode;
+            }
+
+            // Append the new node at the end of the list
+            current.nextNode = node;
+        }
+
+        this.listSize += 1;
+    }
+
+    // Update the value of the corresponding node
+    update(key, value) {
+        let current = this.head;
+
+        // Traverse the list to find the node with the matching key
+        while (current) {
+            if (current.key === key) {
+                // Update the value of the existing node
+                current.value = value;
+            }
+
+            current = current.nextNode;
+        }
+    }
+
+    // Returns true if the passed in value is in the list and otherwise returns false
+    contains(key) {
+        let current = this.head;
+
+        while (current) {
+            if (current.key === key) {
+                return true;
+            }
+
+            current = current.nextNode;
+        }
+
+        return false;
+    }
+
+    // Returns the total number of nodes in this list
+    size() {
+        return this.listSize;
+    }
+
+    toString() {
+        if (this.listSize === 0 || !this.head) {
+            return `Null: Linked list is empty!`;
+        }
+
+        let current = this.head;
+        let string = "";
+
+        while (current) {
+            string += `( ${current.value} ) => `;
+            current = current.nextNode;
+        }
+
+        string += `( ${current} ) `;
+
+        return string;
     }
 }
+
+export default LinkedList;
